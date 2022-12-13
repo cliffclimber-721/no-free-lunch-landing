0be099bfb9f113320d54b6247007b81b5fc459f9
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-12-13 14:17:41 +0900

    ⛏️ Inserted URL 🔨

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const debug = require("debug");
 const { i18n } = require("./next-i18next.config");
 const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const repo = "https://cliffclimber-721.github.io/nofreelunch";
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? `/${repo}/` : "",
   eslint: { ignoreDuringBuilds: true },
   i18n,
   images: {
